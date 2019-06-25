877d60ed8ebe71c0589df28b7e036d8981d15995
zurlomichele <zurlomichele1997@gmail.com>
2019-06-25 23:00:57 +0200

    version 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello!");
-		System.out.println("michele!");
+        System.out.println("Ciao");
+		System.out.println("Michele Zurlo!");
     }
 }
