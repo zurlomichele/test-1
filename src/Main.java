31f14c27754a272e002440b58f6dce187bfca6a2
simone-viozzi <viozzis333@gmail.com>
2019-06-26 11:13:02 +0200

    impara javagit status!

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,25 @@
-public class Main {
-public static void main(String[] args) {
+public class Main 
+{
+	public static void main(String[] args) 
+	{
         System.out.println("Ciao");
         System.out.println("Michele Zurlo!");
-        }
-}
-
-/*public class Prova {
-
-    public static int prova (String args []) {
-
-        for (int x = 1; x < 20; x = x + 1)
-
-            System.out.println("X vale:" + x);
-
+		Prova p = new Prova();
+		p.prova();
     }
-}*/
+		
+
+
+	public class Prova 
+	{
+		public static int prova () 
+		{
+
+		    for (int x = 1; x < 20; x = x + 1)
+			{
+		        System.out.println("X vale:" + x);
+			}
+		}
+	}
+
+}
