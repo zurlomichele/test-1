37a49915922fa7c708babec61e58f46e55eb6e17
zurlomichele <zurlomichele1997@gmail.com>
2019-06-26 11:39:08 +0200

    version 3

---
+++ src/Prova.java
@@ -0,0 +1,9 @@
+public class Prova {
+    public static int prova() {
+
+        for (int x = 1; x < 21; x = x + 1) {
+            System.out.println("X vale:" + x);
+        }
+        return 0;
+    }
+}
