025f4ed0287a3dffdde935e7960df008ae4621b0
zurlomichele <zurlomichele1997@gmail.com>
2019-06-27 15:43:23 +0200

    version 4.2

--- src/Prova.java
+++ src/Prova.java
@@ -1,8 +1,8 @@
 public class Prova {
     public static int prova() {
 
-        for (int x = 1; x < 21; x = x + 1) {
-            System.out.println("X vale:" + x);
+        for (int x = 1; x <=100; x = x + 1) {
+            System.out.println("X vale: " + x);
         }
         return 0;
     }
